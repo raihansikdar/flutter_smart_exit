3223ac55e774a988acaeae524fce923df24b5703
raihansikdar <raihansikdar10@gmail.com>
2025-09-18 10:32:11 +0600

    write package code

--- lib/flutter_smart_exit.dart
+++ lib/flutter_smart_exit.dart
@@ -1,5 +1,317 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+enum ExitOption {bottomSheetExit, popUpExit, backPressExit}
+
+class FlutterSmartExit extends StatefulWidget {
+  final ExitOption exitOption;
+  final String? exitMessage;
+  final TextStyle? exitMessageStyle;
+  final String? cancelButtonText;
+  final TextStyle? cancelButtonTextStyle;
+  final ButtonStyle? cancelButtonStyle;
+  final String? exitButtonText;
+  final TextStyle? exitButtonTextStyle;
+  final ButtonStyle? exitButtonStyle;
+
+  final Color? backgroundColor;
+  final double? bottomSheetHeight;
+  final Widget child;
+
+  @override
+  State<FlutterSmartExit> createState() => _FlutterSmartExitState();
+
+  const FlutterSmartExit({
+    super.key,
+    required this.exitOption,
+    this.exitMessage,
+    this.exitMessageStyle,
+    this.cancelButtonText,
+    this.cancelButtonTextStyle,
+    this.cancelButtonStyle,
+    this.exitButtonText,
+    this.exitButtonTextStyle,
+    this.exitButtonStyle,
+    this.backgroundColor,
+    this.bottomSheetHeight,
+    required this.child,
+
+  });
 }
+
+class _FlutterSmartExitState extends State<FlutterSmartExit> {
+
+  bool canPop = false;
+  int backPressCounter = 0;
+  Timer? backPressTimer;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    return PopScope(
+        canPop: canPop,
+        onPopInvokedWithResult: (didPop, dynamic) {
+          if (canPop) {
+            return; 
+          } else {
+           
+            backPressCounter++;
+
+            if (widget.exitOption == ExitOption.bottomSheetExit) {
+              exitBottomSheet();
+            }else if (widget.exitOption == ExitOption.popUpExit) {
+              exitPopUp(size);
+            }  else if (backPressCounter == 2) {
+              canPop = true;
+              SystemNavigator.pop();
+            } else {
+              backPressTimer?.cancel();
+              backPressTimer = Timer(const Duration(seconds: 2), () {
+                backPressCounter = 0;
+              });
+              exitSnackBar();
+            }
+          }
+        },
+        child: widget.child
+    );
+  }
+
+  void exitPopUp(Size size){
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: widget.backgroundColor ?? Colors.white,
+          contentPadding: EdgeInsets.zero,
+          titlePadding: EdgeInsets.zero,
+          title: Align(
+            alignment: Alignment.center,
+            child: Container(
+              transform: Matrix4.translationValues(0, -size.height * 0.028, 0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  
+                  Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(25),
+                          border: Border.all(color: Colors.red,width: 2)
+                      ),
+                      child: Image.asset("assets/exit.gif",height: size.height * 0.045,),
+                    ),
+                  SizedBox(height: size.height * 0.010),
+                  Text(
+                    widget.exitMessage ?? "Are you ready to exit ?",
+                    style: widget.exitMessageStyle ?? TextStyle(fontWeight: FontWeight.w500,fontSize: 20,color: Colors.grey.shade500),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // content: Text(widget.message),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: widget.cancelButtonStyle ??  ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    side: BorderSide(color: Colors.grey.shade400, width: size.height * 0.001),
+                    minimumSize: size.width < 600 ?   Size(size.width * 0.26, size.height * 0.04) : Size(size.width * 0.025, size.height * 0.050),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    widget.cancelButtonText ?? "Cancel",
+                    style: widget.cancelButtonTextStyle ??  TextStyle(
+                      color: Colors.black,
+                      fontSize: size.height * 0.016,
+                    ),
+                  ),
+                ),
+                SizedBox(width: size.width *0.016,),
+                ElevatedButton(
+                  style:widget.exitButtonStyle ?? ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    side: BorderSide.none,
+                    minimumSize: size.width < 600 ?   Size(size.width * 0.26, size.height * 0.04) : Size(size.width * 0.025, size.height * 0.050),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+
+                  ),
+                  onPressed: () async{
+                    SystemNavigator.pop();
+                  },
+                  child:  Text(
+                    widget.exitButtonText ?? "Exit",
+                    style: widget.exitButtonTextStyle ?? TextStyle(
+                        color: Colors.white,
+                        fontSize: size.height * 0.016,
+                        fontWeight: FontWeight.w700
+                    ),
+                  ),
+                )
+
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void exitSnackBar() {
+    final textLength = widget.exitMessage?.length ?? 25;
+    const minMargin = 45.0;
+    const maxMargin = 110.0;
+
+    // Calculate the margin based on text length
+    double dynamicMargin = (textLength > 30) // You can adjust this threshold
+        ? minMargin
+        : maxMargin;
+
+    final snackBar = SnackBar(
+      content: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text(
+            widget.exitMessage ?? 'Press back again to exit',
+            textAlign: TextAlign.center,
+            style: widget.exitMessageStyle ??  const TextStyle(
+              color: Colors.black,
+              fontSize: 14,
+            ),
+          ),
+        ),
+      ),
+      duration: const Duration(seconds: 2),
+      behavior: SnackBarBehavior.floating,
+      margin: EdgeInsets.only(
+        left: dynamicMargin,
+        right: dynamicMargin,
+        bottom: 30.0,
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 9.0, horizontal: 8.0),
+      backgroundColor: widget.backgroundColor ?? /*Colors.black87.withOpacity(0.6)*/ Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular( dynamicMargin == minMargin ? 50 : 20.0),
+      ),
+      elevation: 8.0,
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void exitBottomSheet() {
+    Size size = MediaQuery.sizeOf(context);
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      showModalBottomSheet(
+        context: context,
+        backgroundColor: widget.backgroundColor ?? Colors.white,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
+          //side: BorderSide(color: Colors.red,width: 3)
+        ),
+        builder: (BuildContext context) {
+          return Container(
+            height: widget.bottomSheetHeight ?? (size.width > 600 ? 215 : 175),
+            transform: Matrix4.translationValues(0, -size.height * 0.040, 0),
+
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                      height: size.width > 500 ? 70: 50,
+                      width: size.width > 500 ? 70 : 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(35),
+                          border: Border.all(color: Colors.red,width: 2)
+                      ),
+                      child: Image.asset("assets/exit.gif",height: size.width > 600 ? size.height * 0.080 : size.height * 0.045,),
+                  ),
+                  SizedBox(height: size.width > 600 ? 10 : 20),
+                  Text(
+                    widget.exitMessage ?? "Are you ready to exit ?",
+                    style: widget.exitMessageStyle ??  TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: size.width < 600 ? 18 : 24,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        style: widget.cancelButtonStyle ?? ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          side: BorderSide(color: Colors.grey.shade400, width: size.height * 0.001),
+                          minimumSize: size.width < 600 ?   Size(size.width * 0.4, size.height * 0.04) : Size(size.width * 0.25, size.height * 0.050),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(
+                          widget.cancelButtonText ?? "Cancel",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: size.height * 0.016,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: size.width *0.016,),
+                      ElevatedButton(
+                        style:widget.exitButtonStyle ??  ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          side: BorderSide.none,
+                          minimumSize: size.width < 600 ?   Size(size.width * 0.4, size.height * 0.04) : Size(size.width * 0.25, size.height * 0.050),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+
+                        ),
+                        onPressed: () async{
+                          SystemNavigator.pop();
+                        },
+                        child:  Text(
+                          widget.exitButtonText ?? "Exit",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: size.height * 0.016,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      )
+
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    });
+  }
+}
+
