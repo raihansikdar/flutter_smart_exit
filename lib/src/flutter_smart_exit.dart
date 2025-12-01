94dc5330ed1e50262714ee695ca8584397035094
raihansikdar <raihansikdar10@gmail.com>
2025-12-01 11:15:17 +0600

    solve bottom sheet error

--- lib/src/flutter_smart_exit.dart
+++ lib/src/flutter_smart_exit.dart
@@ -333,59 +333,64 @@ class _FlutterSmartExitState extends State<FlutterSmartExit> {
                 ),
                 const SizedBox(height: 15),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    ElevatedButton(
-                      style:
-                          widget.cancelButtonStyle ??
-                          ElevatedButton.styleFrom(
-                            backgroundColor: Colors.white,
-                            side: BorderSide(
-                              color: Colors.grey.shade400,
-                              width: size.height * 0.001,
-                            ),
-                            minimumSize: size.width < 550
-                                ? Size(size.width * 0.4, size.height * 0.04)
-                                : Size(size.width * 0.25, size.height * 0.050),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16.0),
-                            ),
-                          ),
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: Text(
-                        widget.cancelButtonText ?? "Cancel",
+                    Expanded(
+                      child: ElevatedButton(
                         style:
-                            widget.cancelButtonTextStyle ??
-                            TextStyle(
-                              color: Colors.black,
-                              fontSize: size.height * 0.016,
+                            widget.cancelButtonStyle ??
+                            ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              side: BorderSide(
+                                color: Colors.grey.shade400,
+                                width: size.height * 0.001,
+                              ),
+                              minimumSize: size.width < 550
+                                  ? Size(size.width * 0.4, size.height * 0.04)
+                                  : Size(size.width * 0.25, size.height * 0.050),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16.0),
+                              ),
                             ),
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: Text(
+                          widget.cancelButtonText ?? "Cancel",
+                          style:
+                              widget.cancelButtonTextStyle ??
+                              TextStyle(
+                                color: Colors.black,
+                                fontSize: size.height * 0.016,
+                              ),
+                        ),
                       ),
                     ),
                     SizedBox(width: size.width * 0.016),
-                    ElevatedButton(
-                      style:
-                          widget.exitButtonStyle ??
-                          ElevatedButton.styleFrom(
-                            backgroundColor: Colors.red,
-                            minimumSize: size.width < 550
-                                ? Size(size.width * 0.4, size.height * 0.04)
-                                : Size(size.width * 0.25, size.height * 0.050),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16.0),
-                            ),
-                          ),
-                      onPressed: () async {
-                        SystemNavigator.pop();
-                      },
-                      child: Text(
-                        widget.exitButtonText ?? "Exit",
+                    Expanded(
+                      child: ElevatedButton(
                         style:
-                            widget.exitButtonTextStyle ??
-                            TextStyle(
-                              color: Colors.white,
-                              fontSize: size.height * 0.016,
-                              fontWeight: FontWeight.bold,
+                            widget.exitButtonStyle ??
+                            ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red,
+                              minimumSize: size.width < 550
+                                  ? Size(size.width * 0.4, size.height * 0.04)
+                                  : Size(size.width * 0.25, size.height * 0.050),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16.0),
+                              ),
                             ),
+                        onPressed: () async {
+                          SystemNavigator.pop();
+                        },
+                        child: Text(
+                          widget.exitButtonText ?? "Exit",
+                          style:
+                              widget.exitButtonTextStyle ??
+                              TextStyle(
+                                color: Colors.white,
+                                fontSize: size.height * 0.016,
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
                       ),
                     ),
                   ],
