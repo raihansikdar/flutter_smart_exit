a0c39eaa7de701ae8ecec31c6bb8a0e9b097965c
raihan sikdar <raihansikdar10@gmail.com>
2025-12-01 08:05:13 +0600

    version 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) 
 
 ---
 
-## [1.0.0] - 2025-11-30
+## [1.0.0] - 2025-12-01
 
 ### Added
 - Initial release of the `flutter_smart_exit` package.
--- example/example.dart
+++ example/example.dart
@@ -13,9 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Exit Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Flutter Smart Exit'),
     );
   }
@@ -31,23 +29,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: FlutterSmartExit(
-        exitType: ExitType.backPressExit,    // Select exit type
+        exitType: ExitType.backPressExit, // Select exit type
         exitImage: Image.asset("gif/exit.gif"), // Add your gif, image or icon
         child: Center(
           child: Column(
             mainAxisAlignment: .center,
             children: [
-              const Text('Home Screen',style: TextStyle(fontSize: 20),),
+              const Text('Home Screen', style: TextStyle(fontSize: 20)),
             ],
           ),
         ),
